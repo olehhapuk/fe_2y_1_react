f426d5f4321be5d5dad60c0edbffeccf523fe829
olehhapuk <oleggapuk20@gmail.com>
2023-05-20 13:20:48 +0300

    repeat

--- src/App.jsx
+++ src/App.jsx
@@ -1,82 +1,20 @@
 import { useState } from 'react';
-import { nanoid } from 'nanoid';
 
-import Container from './components/Container/Container';
-import TaskList from './components/TaskList/TaskList';
-import TaskEditor from './components/TaskEditor/TaskEditor';
-import Input from './components/Input/Input';
-import Section from './components/Section';
+import CounterActions from './components/CounterActions';
 
 function App() {
-  const [tasks, setTasks] = useState([]);
-  const [query, setQuery] = useState('');
+  const [counterValue, setCounterValue] = useState(0);
+  const [step, setStep] = useState(5);
 
-  function createTask(text) {
-    const newTask = {
-      text: text,
-      completed: false,
-      id: nanoid(),
-    };
-
-    setTasks((prevTasks) => [newTask, ...prevTasks]);
+  function increment(step) {
+    setCounterValue((prevCounterValue) => prevCounterValue + step);
   }
-
-  function removeTask(id) {
-    setTasks((prevTasks) => prevTasks.filter((task) => task.id !== id));
-  }
-
-  function updateTask(id, completed) {
-    setTasks((prevTasks) =>
-      prevTasks.map((task) => {
-        if (task.id === id) {
-          const updatedTask = {
-            ...task,
-            completed,
-          };
-
-          return updatedTask;
-        } else {
-          return task;
-        }
-      })
-    );
-  }
-
-  const filteredTasks = tasks.filter((task) => {
-    return task.text.includes(query);
-    // if (task.text.includes(query)) {
-    //   return true;
-    // } else {
-    //   return false;
-    // }
-  });
 
   return (
-    <Container>
-      <Section title="Create todo">
-        <TaskEditor onCreate={createTask} />
-      </Section>
-
-      <Section title="Search">
-        <Input
-          placeholder="Search"
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-        />
-      </Section>
-
-      <Section title="Tasks">
-        {filteredTasks.length > 0 ? (
-          <TaskList
-            tasks={filteredTasks}
-            onRemove={removeTask}
-            onUpdate={updateTask}
-          />
-        ) : (
-          <p>No tasks</p>
-        )}
-      </Section>
-    </Container>
+    <div>
+      <p>{counterValue}</p>
+      <CounterActions increment={increment} />
+    </div>
   );
 }
 
---
+++ src/components/CounterActions.jsx
@@ -0,0 +1,5 @@
+function CounterActions({ increment }) {
+  return <button onClick={() => increment(5)}>+ 5</button>;
+}
+
+export default CounterActions;
