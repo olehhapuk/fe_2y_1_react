10287004c59e3642be0be778e8b9354082ee743e
olehhapuk <oleggapuk20@gmail.com>
2023-05-27 14:26:04 +0300

    request on first render

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,32 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { TailSpin } from 'react-loader-spinner';
+
+import Searchbar from './components/Searchbar';
+import ArticlesList from './components/ArticlesList';
+
 function App() {
-  return <div>App</div>;
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+
+    axios
+      .get('https://hn.algolia.com/api/v1/search?query=react&page=0')
+      .then((res) => {
+        setArticles(res.data.hits);
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return (
+    <div>
+      <Searchbar />
+      <TailSpin visible={loading} />
+      <ArticlesList articles={articles} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Searchbar.jsx
@@ -0,0 +1,10 @@
+function Searchbar() {
+  return (
+    <form>
+      <input type="search" placeholder="Search..." />
+      <button type="submit">Search</button>
+    </form>
+  );
+}
+
+export default Searchbar;
